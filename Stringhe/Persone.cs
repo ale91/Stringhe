f3655661bc5c2de49a6c1c0532cf27b10f914a3f
Aesposto <Aesposto@Z240-02>
2025-03-24 11:45:30 +0100

    Stringhe

---
+++ Stringhe/Persone.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringhe
+{
+    public class Persone
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string CodiceFiscale { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string Indirizzo { get; set; }
+        public string Citta { get; set; }
+        public string Provincia { get; set; }
+        public string CAP { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Note { get; set; }
+        public Persone(string nome, string cognome, string codiceFiscale, DateTime dataNascita, string indirizzo, string citta, string provincia, string cap, string telefono, string email, string note)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            CodiceFiscale = codiceFiscale;
+            DataNascita = dataNascita;
+            Indirizzo = indirizzo;
+            Citta = citta;
+            Provincia = provincia;
+            CAP = cap;
+            Telefono = telefono;
+            Email = email;
+            Note = note;
+        }
+
+        public Persone(string nome, string cogonome, DateTime dataNascita, string indirizzo)
+        {
+            Nome = nome;
+            Cognome = cogonome;
+            DataNascita = dataNascita;
+            Indirizzo = indirizzo;
+
+
+        }
+
+        static void StampaLista(List<Persone> lista)
+        {
+            foreach (Persone p in lista)
+            {
+                Console.WriteLine($"{p.Nome} {p.Cognome}");
+            }
+        }
+
+        
+        /*public override string ToString()
+        {
+            return $"{Nome} {Cognome} - {CodiceFiscale} - {DataNascita.ToShortDateString()} - {Indirizzo} - {Citta} - {Provincia} - {CAP} - {Telefono} - {Email} - {Note}";
+        }
+        */
+    }
+}
--- Stringhe/Program.cs
+++ Stringhe/Program.cs
@@ -6,40 +6,118 @@ using System.Threading.Tasks;
 
 namespace Stringhe
 {
+
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+        public DateTime DataNascita { get; set; }
+
+        public string CodiceFiscale { get; set; }
+
+        public Persona (string nome, string cognome, DateTime dataNascita, string codiceFiscale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} {Cognome}, Nato il: {DataNascita.ToShortDateString()}, CodiceFiscale: {CodiceFiscale}";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var persone = new List<Persone>(); //creo lista
 
-            //Creazione lista di stringhe
-            List<string> listaStringhe = new List<string>();
+            var Fabio = new Persone("Fabio", "Farinola", new DateTime(1989,09,27), "abcd"); //creazione persona
+            var Mario = new Persone("Mario", "Rossi", new DateTime(1990, 10, 27), "abcd");
+            var Luigi = new Persone("Luigi", "Verdi", new DateTime(1991, 08, 18), "abcd");
+            var Luca = new Persone("Luca", "Diblasio", new DateTime(1992, 01, 12), "abcd");
 
-            //Aggiunta di elementi alla lista
-            listaStringhe.Add("Ciao");
-            listaStringhe.Add("Mondo");
-            listaStringhe.Add("!");
-            Console.WriteLine("Lista di stringhe:");
-            StampaLista(listaStringhe);
+            persone.Add(Fabio); //aggiungo persona alla lista
+            persone.Add(Mario);
+            persone.Add(Luigi);
+            persone.Add(Luca);
 
-            //Modifica di un elemento della lista
-            listaStringhe[1] = "!!!";
-            Console.WriteLine("\nLista di stringhe modificata:");
-            StampaLista(listaStringhe);
+            /*
+            //where
+            var t = persone.Where(x => x.Nome.Contains("L")).ToList();
 
-            //Rimozione di un elemento della lista, esempio rimozione primo elemento
-            listaStringhe.RemoveAt(0);
-            Console.WriteLine("\nLista di stringhe con primo elemento rimosso:");
-            StampaLista(listaStringhe);
+            //ForE\ach
+            persone.ForEach(x => x.CAP = "76016");
 
+            var t2 = persone.Select(x => x.Cognome);
+            */
+
+            Console.WriteLine("Lista inizale:");
+            StampaPersone(persone);
+
+            //Aggiungi nuovo oggetto alla lista
+            var Anna = new Persona("Anna", "Bianchi", new DateTime(1993, 05, 21), "efgh");
+            persone.Add(Anna);
+            Console.WriteLine("\nDopo aggiunta di Anna:");
+
+            //Modifica un oggetto della lista
+            var personaDaModificare = persone.FirstOrDefault(p => p.Nome == "Luigi");
+            if(personaDaModificare != null)
+            {
+                personaDaModificare.Cognome = "Rossi";
+                personaDaModificare.CodiceFiscale = "ijkl";
+            }
+            Console.WriteLine("\nDopo modifica di Luigi:");
+            StampaPersone(persone);
+
+            //Rimuovi Mario
+            RimuoviPersona(persone, "Mario");
+            Console.WriteLine("\nLista dopo rimozione:");
+            StampaPersone(persone);
+
+            //Ordina la lista per nome
+            var personeOrdinate = OrdinaPersonePerNome(persone);
+            
         }
 
         //Stampa tutti gli elementi della lista
-        static void StampaLista(List<string> lista)
+        static void StampaPersone(List<Persone> persone)
         {
-            foreach (string s in lista)
+            //Intestazione tabella
+            Console.WriteLine("{0, -10}{1 -10}{2 -20}{3 -20}");
+            Console.WriteLine(('-', 30));
+
+            //formato tabellare
+            foreach (var persona in persone)
             {
-                Console.WriteLine(s);
+                Console.WriteLine("{0, -10}{1 -10}{2 -20}{3 -20}", persona.Nome, persona.Cognome, persona.DataNascita, persona.CodiceFiscale);
             }
+        }
+
+        //Metodo per rimuovere persona da lista in base al nome
+        static void RimuoviPersona(List<Persone> persone, string nome)
+        {
+            //Trova persona con nome specificato
+            var personaDaRimuovere = persone.FirstOrDefault(p => p.Nome.Equals(nome, StringComparison.OrdinalIgnoreCase));
+            if (personaDaRimuovere!=null)
+            {
+                persone.Remove(personaDaRimuovere);
+                Console.WriteLine($"\n'{nome}' {personaDaRimuovere.Cognome} {personaDaRimuovere.Nome}è stato rimosso dall'elenco.");
+            }
+            else
+            {
+                Console.WriteLine($"\nNessuna persona con il nome '{nome}' è nell'elenco.");
+            }
+        }
+
+        //Metodo per ordinare la lista in base al nome
+        static List<Persone> OrdinaPersonePerNome(List<Persone> persone)
+        {
+            return persone.OrderBy(p => p.Nome).ToList(); //ogni elemento di p nella lista persone, ordina in base alla proprietà Nome
         }
     }
 
